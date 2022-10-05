bf8ed546439673bd53c20b6ee32d95de45972378
mollikacomputer <mollikacomputer3@gmail.com>
2022-10-05 21:35:20 +0600

    dbConnect file export and import into app file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const dbConnect = require('./utils/dbConnect');
 
 
 // middleware
@@ -10,6 +11,7 @@ app.use(cors());
 
 // schema desigh
 
+dbConnect();
 
 app.get('/', (req, res) =>{
     res.send('schema design App is running')
