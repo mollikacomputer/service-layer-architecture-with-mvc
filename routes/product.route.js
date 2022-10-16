d454f8d8e70d33dd2595a5fcdfbe713fbe1a2b75
mollikacomputer <mollikacomputer3@gmail.com>
2022-10-16 23:06:46 +0600

    at last give dynamic route

--- routes/product.route.js
+++ routes/product.route.js
@@ -12,7 +12,7 @@ router.route('/bulk-update')
 .patch(productController.bulkUpdateProduct)
 
 // update product dynamic id bulk update ar pore  pore dite hobe
+// ডায়নামিক ্আইডি সবার শেষে দিতে হবে
 router.route('/:id')
 .patch(productController.updateProduct)
-
 module.exports = router;
